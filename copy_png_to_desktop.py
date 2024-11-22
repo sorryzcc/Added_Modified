13e3093b6edca8c664caee7915b91dd4eee253bc
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2024-11-22 17:27:38 +0800

    修改copu_png_to_desktop.py

--- copy_png_to_desktop.py
+++ copy_png_to_desktop.py
@@ -1,7 +1,11 @@
 import os
 import shutil
+import pandas as pd
 
-def copy_png_files(source_file, target_dir, max_count=1000):
+def copy_png_files(input_file, target_dir, max_count=1000):
+    # 读取Excel文件
+    df = pd.read_excel(input_file)
+    
     # 确保目标目录存在，如果不存在则创建
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
@@ -12,36 +16,37 @@ def copy_png_files(source_file, target_dir, max_count=1000):
     current_folder = os.path.join(target_dir, f'folder_{folder_index}')
     os.makedirs(current_folder, exist_ok=True)
     
-    # 获取文件名
-    file_name = os.path.basename(source_file)
-    
-    # 构建目标路径
-    target_path = os.path.join(current_folder, file_name)
-    
-    # 检查源文件是否存在
-    if os.path.exists(source_file):
-        # 拷贝文件
-        shutil.copy2(source_file, target_path)
-        print(f"Copied {source_file} to {target_path}")
-    else:
-        print(f"Source file not found: {source_file}")
-    
-    # 更新计数器
-    file_count += 1
-    
-    # 如果当前文件夹中的文件数量达到最大值，创建新的文件夹
-    if file_count >= max_count:
-        folder_index += 1
-        current_folder = os.path.join(target_dir, f'folder_{folder_index}')
-        os.makedirs(current_folder, exist_ok=True)
-        file_count = 0
+    # 遍历所有路径
+    for _, row in df.iterrows():
+        source_path = row['Path']
+        
+        # 检查文件是否存在
+        if os.path.exists(source_path):
+            # 构建目标路径
+            target_path = os.path.join(current_folder, os.path.basename(source_path))
+            
+            # 拷贝文件
+            shutil.copy2(source_path, target_path)
+            print(f"Copied {source_path} to {target_path}")
+        else:
+            print(f"Source file not found: {source_path}")
+        
+        # 更新计数器
+        file_count += 1
+        
+        # 如果当前文件夹中的文件数量达到最大值，创建新的文件夹
+        if file_count >= max_count:
+            folder_index += 1
+            current_folder = os.path.join(target_dir, f'folder_{folder_index}')
+            os.makedirs(current_folder, exist_ok=True)
+            file_count = 0
 
 # 主函数
 if __name__ == "__main__":
-    source_file = r'I:\PM_Mainland_Trunk_20230321_r552586\PMGameClient\PMGame\CDNRes\Dev\B02\Default\Mall\Player\Bottom\t_Bottom_male_82.png'
+    input_file = 'updated_processed_svn_log.xlsx'
     target_dir = os.path.expanduser('~/Desktop/PNG_Files')
     
     # 拷贝PNG文件
-    copy_png_files(source_file, target_dir)
+    copy_png_files(input_file, target_dir)
     
     print(f"PNG文件已成功拷贝到 {target_dir}")
